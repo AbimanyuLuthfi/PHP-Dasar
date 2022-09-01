783410f9cb6c128625a4144dc8a41ee39dd87a83
AbimanyuLuthfi <89892701+AbimanyuLuthfi@users.noreply.github.com>
2022-09-02 01:04:11 +0700

    Add files via upload

---
+++ Tipe Data PHP/tipeDataBoolean.php
@@ -0,0 +1,10 @@
+<?php
+    /*
+    Merupakan Tipe Data yang hanya berisi benar dan salah.
+    */
+    echo "Benar : ";
+    var_dump(true); // bernilai benar
+
+    echo "Salah : ";
+    var_dump(false); // bernilai salah
+?>
