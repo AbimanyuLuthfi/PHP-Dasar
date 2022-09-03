837f0800f5a8c71584fc4a1da8b7481295df9c66
AbimanyuLuthfi <89892701+AbimanyuLuthfi@users.noreply.github.com>
2022-09-03 12:55:32 +0700

    Add files via upload

---
+++ Heredoc.php
@@ -0,0 +1,11 @@
+<?php
+    // Heredoc digunakan dengan tag dan diakhiri dengan tag yang sama
+    // Dalam Heredoc kita tidak perlu mengetik ENTER untuk menambahkan baris baru
+    // Kita juga bisa menggunakan Double Quote dalam Heredoc.
+    
+    echo <<<ABI
+    ini adalah contoh string yang sangat,
+    panjang, dan juga tidak perlu mengetik "ENTER" secara
+    manual, "bisa quote" juga. 
+    ABI;
+?>
