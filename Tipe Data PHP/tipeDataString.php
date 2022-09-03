1913c20171afc4800ac8477caeeceb052a1ca448
AbimanyuLuthfi <89892701+AbimanyuLuthfi@users.noreply.github.com>
2022-09-03 12:40:32 +0700

    Add files via upload

---
+++ Tipe Data PHP/tipeDataString.php
@@ -0,0 +1,18 @@
+<?php
+    // Tipe Data String pada bahasa Pemrograman PHP, dapat menggunakan single quote ''
+
+    echo 'Nama : ';
+    echo 'Abimanyu Luthfi Rizq Ramadhan';
+
+    // Selain menggunakan single quote, 
+    // tipe data string juga bisa menggunakan double quote ""
+    echo "Nama : ";
+    echo "Abimanyu\t Luthfi\t Rizq\t Ramadhan";
+
+    // Perintah dibawah ini akan tetap sama
+    echo '\n';
+
+    // Jika kita ingin mencetak syntax diatas menjadi breakline, maka harus menggunakan double quote.
+    // Seperti program dibawah ini
+    echo "\n";
+?>
