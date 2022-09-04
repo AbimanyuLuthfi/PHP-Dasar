e573f9e9f772be427712bba4cd9e2e89bd53f0a3
AbimanyuLuthfi <89892701+AbimanyuLuthfi@users.noreply.github.com>
2022-09-04 16:19:24 +0700

    Add files via upload

---
+++ constant.php
@@ -0,0 +1,19 @@
+<?php
+    /*
+        Constant merupakan suatu wadah untuk menyimpan data
+        yang tidak bisa dirubah lagi setelah di deklarasikan
+    */
+    // Dalam membuat constant kita bisa menggunakan function define()
+
+    define("AUTHOR", "Abimanyu Luthfi Rizq Ramadhan");
+    define("APP_VERSION", 100);
+
+    echo "App Author : ";
+    echo AUTHOR;
+    echo "\n";
+
+    echo "App Version : ";
+    echo APP_VERSION;
+    echo "\n";
+
+?>
