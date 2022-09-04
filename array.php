d9f9df277cdc975da6183b9636955a30a9debf8b
AbimanyuLuthfi <89892701+AbimanyuLuthfi@users.noreply.github.com>
2022-09-04 21:31:46 +0700

    Add files via upload

---
+++ array.php
@@ -0,0 +1,11 @@
+<?php
+    // Membuat Array dengan cara-1
+    $values = array(1, 2, 3, 4, 7.5);
+    var_dump($values);
+
+    // Membuat Array dengan cara-2
+    $name = ["Abimanyu", "Luthfi", "Rizq", "Ramadhan", 19];
+    var_dump($name);
+
+    echo $name[4]; // Mengakses/melihat array pada index ke-5 di variable "name".
+?>
