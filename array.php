a3c0edd58bfff7c6f5926a7113e191a247bbf40c
AbimanyuLuthfi <89892701+AbimanyuLuthfi@users.noreply.github.com>
2022-09-04 21:44:54 +0700

    Update array.php

--- array.php
+++ array.php
@@ -7,5 +7,22 @@
     $name = ["Abimanyu", "Luthfi", "Rizq", "Ramadhan", 19];
     var_dump($name);
 
-    echo $name[4]; // Mengakses/melihat array pada index ke-5 di variable "name".
+    // Melakukan check pada array pertama/index pertama.
+    var_dump($name[0]);
+    
+    // Mengubah nilai array 
+    $name[0] = "Burhan";
+    echo $name[0]; // Menampilkan nilai baru pada index ke-0 setelah nilainya diubah.
+    var_dump($name[0]);
+
+    // Menghapus data pada index array.
+    unset($name[0]); // Menghapus Array pada index-0
+    var_dump($name); // Melakukan check kembali 
+
+    // Menambahkan array
+    // Array tersebut akan ditambahkan pada index terakhir
+    $name[] = "Reree";
+    var_dump($name); // Melakukan check kembali pada array tersebut.
+
+    var_dump(count($name));
 ?>
