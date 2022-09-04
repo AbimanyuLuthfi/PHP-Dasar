9f102862ef41382d5e04c235311a6ca985a41899
AbimanyuLuthfi <89892701+AbimanyuLuthfi@users.noreply.github.com>
2022-09-04 13:05:16 +0700

    Add files via upload

---
+++ Variable/variableVariables.php
@@ -0,0 +1,14 @@
+<?php
+// variable Variables merupakan sebuah fitur bahasa pemrograman PHP
+// Untuk membuat variable baru dari nilai variable sebelumnya.
+
+    $name = "Abi";
+    $$name = "Merupakan Mahasiswa Universitas Pembangunan Jaya"; //akan membuat variable baru dari isi variable "name"
+
+    echo "\$name  ";
+    echo $name;
+    echo "\n";
+    echo "\$Abi = ";
+    echo $Abi;
+    echo "\n";
+?>
